d46d6433c029163726f7ea2b7c0313511e4644e4
abdelbasset <abdelbassetselmi022@gmail.com>
2024-10-15 13:15:09 +0100

    trr

---
+++ server/database/index.js
@@ -0,0 +1,15 @@
+const mongoose = require("mongoose");
+
+const url = "mongodb://127.0.0.1:27017/user";
+
+mongoose
+  .connect(url, {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+  })
+  .then(() => {
+    console.log("Connected to MongoDB");
+  })
+  .catch((err) => {
+    console.error("Error connecting to MongoDB:", err);
+  });
--- server/server.js
+++ server/server.js
@@ -7,18 +7,4 @@ const app = express();
 app.use(express.json());
 app.use(cors());
 
-const url = "mongodb://127.0.0.1:27017/user";
-
-mongoose
-  .connect(url, {
-    useNewUrlParser: true,
-    useUnifiedTopology: true,
-  })
-  .then(() => {
-    console.log("Connected to MongoDB");
-  })
-  .catch((err) => {
-    console.error("Error connecting to MongoDB:", err);
-  });
-
 app.listen(3000, () => console.log("Server running on port 3000"));
