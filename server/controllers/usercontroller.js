44dced4d383eba24edb621149362cc65b5275807
nextwarrior000 <yassinesalem543@gmail.com>
2024-10-16 09:33:22 +0100

    correct

--- server/controllers/usercontroller.js
+++ server/controllers/usercontroller.js
@@ -1,26 +1,59 @@
-const User = require("../models/users");
-const createUser = async (req, res) => {
+
+const express = require("express");
+const router = express.Router();
+const User = require("../models/User");
+const bcrypt = require("bcryptjs");
+const jwt = require("jsonwebtoken");
+
+// POST route to create a new user
+const createNewUser = async (req, res) => {
+  const { data } = req.body;
+  const user = new User(data);
+  const salt = bcrypt.genSaltSync(10);
+  const cryptedPassword = await bcrypt.hashSync(data.password, salt);
+  user.password = cryptedPassword;
+
   try {
-    const { firstName, lastName, address, password } = req.body;
-    const newUser = new User({ firstName, lastName, address, password });
-    await newUser.save();
-    res
-      .status(201)
-      .json({ message: "User created successfully", user: newUser });
+    const savedUser = await user.save();
+    res.status(200).send(savedUser);
   } catch (err) {
-    res.status(500).json({ error: "Error creating user" });
-  }
-};
-const getUsers = async (req, res) => {
-  try {
-    const users = await User.find();
-    res.status(200).json(users);
-  } catch (err) {
-    res.status(500).json({ error: "Error fetching users" });
+    res.send(err);
   }
 };
 
-module.exports = {
-  createUser,
-  getUsers,
+// POST route to log in a user
+const    loginUser = async (req, res) => {
+  const { data } = req.body;
+  const user = await User.findOne({ email: data.email });
+
+  if (!user) {
+    return res.status(404).send("Email or password is incorrect");
+  }
+
+  const validPassword = await bcrypt.compareSync(data.password, user.password);
+  if (!validPassword) {
+    return res.status(404).send("Email or password is incorrect");
+  }
+
+  const payload = {
+    _id: user._id,
+    email: user.email,
+    name: user.name,
+  };
+
+  const token = jwt.sign(payload, "123457");
+  res.status(200).send({ myToken: token });
 };
+
+// GET route to retrieve all users
+router.get("/getall", async (req, res) => {
+  try {
+    const users = await User.find();
+    res.send(users);
+  } catch (err) {
+    res.send(err);
+  }
+});
+
+module.exports = { createNewUser, loginUser };
+
